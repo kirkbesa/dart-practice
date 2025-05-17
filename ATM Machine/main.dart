7f8ba21a97987f6d4abe66c10c3b7fce98008eae
Student <student@172-15-3-43.lightspeed.stlsmo.sbcglobal.net>
2025-05-17 17:46:31 +0800

    minor changes

---
+++ ATM Machine/main.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+void main() {
+  var InitialPin = 0000;
+
+
+}
+
+void EnterPin() {
+
+}
+
+int BalanceInquiry() {
+  return 0;
+}
+
+void WithdrawCash() {
+
+}
+
+void ChangePin() {
+
+}
+
+void PayBills() {
+
+}
+
+void DepositMoney() {
+
+}
+
+String? AskInput() {
+  
+  if (stdin.readLineSync() == 'exit') {
+    return ';
+}
